8770f5e27a66db4ada94ec3eb354a921d72f0aeb
KEN-1204 <cieletoile.1204@gmail.com>
2023-07-03 05:42:15 +0900

    added dashboard-layout

---
+++ components/DashboardLayout.tsx
@@ -0,0 +1,66 @@
+import useStore from "@/store";
+import Head from "next/head";
+import React, { FC, ReactNode, useEffect } from "react";
+import { Modal } from "./Modal/Modal";
+import { useRouter } from "next/router";
+
+type Prop = {
+  title: string;
+  children: ReactNode;
+};
+
+// 各ページをラップして、各ページ毎にCSSクラスやタイトル、ヘッダーなどを柔軟に設定する
+// 各ページのJSXの一番外側に配置
+export const DashboardLayout: FC<Prop> = ({ children, title = "TRUSTiFY | Home" }) => {
+  const theme = useStore((state) => state.theme);
+  const setTheme = useStore((state) => state.setTheme);
+  const changeTheme = () => {
+    if (theme === "light") setTheme("dark");
+    if (theme === "dark") setTheme("light");
+  };
+  // モーダルが開いている時はbodyにoverflow: hiddenを設定する
+  const isOpenModal = useStore((state) => state.isOpenModal);
+  const openLangTab = useStore((state) => state.openLangTab);
+  useEffect(() => {
+    if (isOpenModal || openLangTab) {
+      // モーダルが開いているときに、bodyにoverflow: hiddenを設定
+      document.body.style.overflow = "hidden";
+    } else {
+      // モーダルが閉じているときに、bodyのoverflowを初期状態に戻す
+      document.body.style.overflow = "unset";
+    }
+
+    // useEffectのクリーンアップ関数で、コンポーネントのアンマウント時にも初期状態に戻す
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpenModal, openLangTab]);
+
+  const router = useRouter();
+  const root = router.pathname;
+  console.log("root", root);
+  console.log("root === '/'", root === "/");
+
+  return (
+    <div className={``}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      {/* <header></header> */}
+      {/* <main className="">{children}</main> */}
+      <div className="relative flex h-full min-h-screen flex-col items-center">{children}</div>
+      {/* <footer></footer> */}
+
+      {/* テーマ切り替えボタン */}
+      <div className="flex-center fixed bottom-[2%] right-[1%] h-[10%] w-[10%]">
+        <div
+          className="h-[50px] w-[50px] cursor-pointer rounded-full bg-[--color-bg-brand05]"
+          onClick={changeTheme}
+        ></div>
+      </div>
+
+      {/* モーダル */}
+      <Modal />
+    </div>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,13 @@ import "@/styles/globals.css";
 import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import { Session, SessionContextProvider, useSession } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RootLayout } from "@/components/RootLayout";
 // プログレスバー
 import ProgressBar from "@badrap/bar-of-progress";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import useStore from "@/store";
 
 const queryClient = new QueryClient({
@@ -22,15 +22,15 @@ const queryClient = new QueryClient({
 });
 
 // プログレスバー
-const progress = new ProgressBar({
-  size: 4,
-  color: "#0066ff",
-  className: "z-50",
-  delay: 100,
-});
-Router.events.on("routeChangeStart", progress.start);
-Router.events.on("routeChangeComplete", progress.finish);
-Router.events.on("routeChangeError", progress.finish);
+// const progress = new ProgressBar({
+//   size: 4,
+//   color: "#0066ff",
+//   className: "z-50",
+//   delay: 100,
+// });
+// Router.events.on("routeChangeStart", progress.start);
+// Router.events.on("routeChangeComplete", progress.finish);
+// Router.events.on("routeChangeError", progress.finish);
 
 export default function App({
   Component,
@@ -41,15 +41,17 @@ export default function App({
   const sessionState = useStore((state) => state.sessionState);
   const setSessionState = useStore((state) => state.setSessionState);
   const session = useSession();
+  const currentPathRef = useRef("/");
+  const router = useRouter();
 
   // Create a new supabase browser client on every first render.
   // 初回レンダリングごとに、新しいsupabaseブラウザークライアントを作成します。
-  const [supabaseClient] = useState(() => createPagesBrowserClient());
+  const [supabase] = useState(() => createPagesBrowserClient());
 
   // ユーザーのセッション情報を取得と監視
   useEffect(() => {
     const getUserProfile = async () => {
-      const { data } = await supabaseClient.auth.getSession();
+      const { data } = await supabase.auth.getSession();
       // if (data.session) {
       //   const { data: profile, error } = await supabaseClient
       //     .from("profiles")
@@ -64,13 +66,106 @@ export default function App({
       console.log("getSession()の実行結果で取得したsession", data);
     };
     getUserProfile();
-    console.log("useSession()の実行結果で取得したsession", session);
+
+    // ページ遷移が完了した時のURLを取得 =================================
+    // const handleRouteChange = (url: string) => {
+    //   console.log("App is changing to: ", url);
+    //   currentPathRef.current = url;
+    // };
+    // router.events.on("routeChangeComplete", handleRouteChange);
+    // ==============================================================
+
+    // ユーザーのプロフィール情報のリアルタイム変更を監視
+    const {} = supabase.auth.onAuthStateChange(async (authChangeEvent, currentSession) => {
+      console.log("🌟_app onAuthStateChange authChangeEvent", authChangeEvent);
+      // 認証に成功し、セッションが存在する場合のルート
+      if (currentSession) {
+        // ZustandのセッションStateに現在のセッションを格納
+        console.log("🌟_app onAuthStateChange 取得したセッションをZustandに格納");
+        setSessionState(currentSession);
+
+        /**認証成功後にブラウザバックでGoogleの認証ページに戻ることが無いように
+         * 一度現在の'/'から同じ'/'ルートパスにreplaceで置き換えて
+         * 認証成功しセッションが付与されたら'/home'パスへ自動的に遷移 */
+        // try {
+        //   console.log('🌟router.replace 同じルートに書き換え')
+        //   await router.replace(router.pathname)
+        // } catch(error) {
+        //   console.log('❌_app onAuthStateChange router.replace失敗')
+        // }
+        if (window.history.state.url !== "/home") {
+          console.log("🌟_app onAuthStateChange セッション有り homeにプッシュ", currentSession);
+          console.log(
+            "現在のパス pathname, currentPathRef, window.history.state",
+            router.pathname,
+            // currentPathRef.current,
+            window.history.state.url
+          );
+          await router.push("/home");
+        }
+        console.log("🌟_app onAuthStateChange セッション有り /homeのためそのまま", currentSession);
+        console.log(
+          "現在のパス pathname, currentPathRef, window.history.state",
+          router.pathname,
+          // currentPathRef.current,
+          window.history.state.url
+        );
+      }
+      // ログアウトされセッションが削除されたルート
+      else {
+        // ZustandのセッションStateをnullでリセット
+        console.log("🌟_app onAuthStateChange ログアウトしたためセッションをnullに");
+        setSessionState(null);
+
+        // ログアウト後セッションが削除されたら、自動的に'/'ルートパスに遷移
+        if (window.history.state.url !== "/") {
+          console.log("🌟_app onAuthStateChange セッション無し /へプッシュ", currentSession);
+          console.log(
+            "現在のパス pathname, currentPathRef, window.history.state",
+            router.pathname,
+            // currentPathRef.current,
+            window.history.state.url
+          );
+          await router.push("/");
+        }
+        console.log("🌟_app onAuthStateChange セッション有り /homeのためそのまま", currentSession);
+        console.log(
+          "現在のパス pathname, currentPathRef, window.history.state",
+          router.pathname,
+          // currentPathRef.current,
+          window.history.state.url
+        );
+      }
+    });
   }, []);
 
-  // ユーザーのプロフィール情報のリアルタイム変更を監視
+  useEffect(() => {
+    const progress = new ProgressBar({
+      size: 4,
+      color: "#2B7FFF",
+      // color: "#0066ff",
+      className: "z-50",
+      delay: 100,
+    });
+
+    const handleRouteChangeStart = () => progress.start();
+    const handleRouteChangeComplete = () => progress.finish();
+    const handleRouteChangeError = () => progress.finish();
+
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeError);
+
+    // Appコンポーネントがアンマウントされたときにイベントリスナーをクリーンアップします
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeError);
+    };
+  }, []); // 空の依存配列を指定することで、このuseEffectは一度だけ実行されます
 
   return (
-    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <QueryClientProvider client={queryClient}>
         <RootLayout>
           <Component {...pageProps} />
