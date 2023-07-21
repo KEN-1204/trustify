8b0246fbd7037173a0a123cf883203ec3a31dc42
KEN-1204 <cieletoile.1204@gmail.com>
2023-07-21 09:48:41 +0900

    run build

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -3,18 +3,18 @@
 // プロバイダ（Google、github）から送られてくる一時的なトークン交換用コードをアクセストークンとリフレッシュトークンに
 // 交換してCookieにセットするプロセスを自分自身でカスタムして実装したい場合には、このAPIルートを使用する
 
-import { NextApiHandler } from "next";
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+// import { NextApiHandler } from "next";
+// import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 
-const handler: NextApiHandler = async (req, res) => {
-  const { code } = req.query;
+// const handler: NextApiHandler = async (req, res) => {
+//   const { code } = req.query;
 
-  if (code) {
-    const supabase = createPagesServerClient({ req, res });
-    await supabase.auth.exchangeCodeForSession(String(code));
-  }
+//   if (code) {
+//     const supabase = createPagesServerClient({ req, res });
+//     await supabase.auth.exchangeCodeForSession(String(code));
+//   }
 
-  res.redirect("/");
-};
+//   res.redirect("/");
+// };
 
-export default handler;
+// export default handler;
